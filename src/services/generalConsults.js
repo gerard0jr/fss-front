8cbdc83d79e93ff860c36b9bb87d02ca182f58f2
Gerardo <gera@MacBook-de-Gerardo.local>
2019-02-27 13:33:17 -0600

    Production URL: generalConsult

--- src/services/generalConsults.js
+++ src/services/generalConsults.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const url = 'http://localhost:3000/general'
-// const url = 'https://crm-fss.herokuapp.com/general'
+// const url = 'http://localhost:3000/general'
+const url = 'https://crm-fss.herokuapp.com/general'
 
 export const getSellers = () => 
     axios.get(`${url}/getSellers`)
