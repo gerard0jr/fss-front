d68f59a2b0735b3c10637daf82f74d5115d53843
Gerardo <gera@MacBook-de-Gerardo.local>
2019-05-14 13:43:27 -0500

    Links for AWS EC2

--- src/services/generalConsults.js
+++ src/services/generalConsults.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // const url = 'http://localhost:3000/general'
 // const url = 'https://crm-fss.herokuapp.com/general'
-const url = 'https://3.14.70.148/general'
+const url = 'http://3.14.70.148/general'
 
 export const getSellers = () => 
     axios.get(`${url}/getSellers`)
--- src/services/quotations.js
+++ src/services/quotations.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // const url = 'http://localhost:3000/quotations'
 // const url = 'https://crm-fss.herokuapp.com/quotations'
-const url = 'https://3.14.70.148/quotations'
+const url = 'http://3.14.70.148/quotations'
 
 
 export const getQuot = id =>
